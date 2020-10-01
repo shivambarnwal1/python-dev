c5429294f03291579616380f11ee2f2539e3ec6d
hacking-empower <71137491+hacking-empower@users.noreply.github.com>
2020-10-01 08:49:42 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,7 @@
-print("NOTE: a dictionary of four words only (dry,set,abandon,machine)")
+print("NOTE: This is a dictionary of only four words (dry,set,abandon,machine)")
 
 d1={"dry":"सूखा", "set":"नबंरों का समूह","abandon":"खडंर", "machine":"यत्रं"}
 
-while(63==63):
+while(True):
 	a1=input("enter a word:")
 	print(d1.get(a1))
