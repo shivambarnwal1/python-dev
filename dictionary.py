7f822d9574e50a6997bcc9642b08e328f62a0498
shivambarnwal1 <67702962+shivambarnwal1@users.noreply.github.com>
2020-10-01 06:25:35 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+print("NOTE: a dictionary of four words only (dry,set,abandon,machine)")
+
+d1={"dry":"सूखा", "set":"नबंरों का समूह","abandon":"खडंर", "machine":"यत्रं"}
+
+while(63==63):
+	a1=input("enter a word:")
+	print(d1.get(a1))
